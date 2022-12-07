60ed8da739d7791f99fe8c2542f00d02982aef96
Conrad Waako K <68024054+elconrad23@users.noreply.github.com>
2022-12-07 15:18:13 +0200

    Update ColumnsScreen.dart

--- lib/screens/ColumnsScreen.dart
+++ lib/screens/ColumnsScreen.dart
@@ -1,34 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'ContainerScreen.dart';
 
-class ColumnScreen extends StatefulWidget {
-  const ColumnScreen({super.key});
+class ColumnsScreen extends StatefulWidget {
+  const ColumnsScreen({super.key});
 
   @override
-  State<ColumnScreen> createState() => _ColumnScreenState();
+  State<ColumnsScreen> createState() => _ColumnsScreenState();
 }
 
-class _ColumnScreenState extends State<ColumnScreen> {
+class _ColumnsScreenState extends State<ColumnsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Columns'),
-
-    ),
-     body:
-     //InkWell()
-    // onTap: (){
-     
-     Column(
-    mainAxisAlignment: mainAxisAlignment.center,
-      children:[
-        Text("Sample Question Topic #1"),
-        Text("Sample Question Topic #2"),
-        Text("Sample Question Topic #3"),
-        Text("Sample Question Topic #4"),
-        Text("Sample Question Topic #5"),
-      ].
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Sample Question Topic #1"),
+          ListTile(
+            title: const Text("Land Title Registration"),
+            leading: const Icon(Icons.view_column_outlined),
+            trailing: const Icon(Icons.chevron_right_rounded),
+            subtitle: const Text("All about land registration in Ugandaa..."),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ContainerScreen(),
+                ),
+              );
+            },
+          ),
+          // Text("Sample Question Topic #2"),
+          // Text("Sample Question Topic #3"),
+      
+        ],
       ),
     );
   }
