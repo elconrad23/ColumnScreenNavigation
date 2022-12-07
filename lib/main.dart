3a34d5d339a78cbd122d508f74d202a1e460c6ca
Conrad Waako K <68024054+elconrad23@users.noreply.github.com>
2022-12-07 15:13:44 +0200

    Create main.dart
    Task 3 with Container and text styling done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'screens/ColumnsScreen.dart';
+import 'screens/ContainerScreen.dart';
+import 'screens/RowsScreen.dart';
+import 'screens/imagesScreen.dart';
+import 'screens/ContainerStylingScreen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,39 +16,113 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: _title,
-      home: MyStatefulWidget(),
+      home: MyStatefulWidget(
+        title: _title,
+      ),
     );
-   
   }
 }
 
 class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key, required this.title});
+  // static const String title = 'Flutter Code Sample';
+
+  const MyStatefulWidget({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
 
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:Text("Home Screen"),
-      ),
-      body: InkWell(
-        OnTap(){
-          Navigator.push(contect, MaterialPageRoute(builder: (context) => ColumnScreen(),
-           ),
-          );
-        },
-        Center(
-        Text("Columns"),
-        ),
-    ),
-    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text("Home Screen"),
+    ),
+    body: ListView(
+      children: [
+        ListTile(
+          title: const Text("Columns"),
+          leading: const Icon(Icons.view_column_outlined),
+          trailing: const Icon(Icons.chevron_right_rounded),
+          subtitle: const Text("All about land purchasing in Ugandaa..."),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ColumnsScreen(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          title: const Text("Rows"),
+          leading: const Icon(Icons.table_rows_outlined),
+          trailing: const Icon(Icons.chevron_right_sharp),
+          subtitle: const Text("All about rows..."),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const RowsScreen(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          title: const Text("Text styling"),
+          leading: const Icon(Icons.text_fields_outlined),
+          trailing: const Icon(Icons.chevron_right_sharp),
+          subtitle: const Text("Styling the text..."),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ImagesScreen(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          title: const Text("Container styling"),
+          leading: const Icon(Icons.text_fields_outlined),
+          trailing: const Icon(Icons.chevron_right_sharp),
+          subtitle: const Text("Styling the text..."),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ContainerStylingScreen(),
+              ),
+            );
+          },
+        ),
+        InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ContainerScreen(),
+              ),
+            );
+          },
+        ),
+      ],
+      // Center(
+      // Text("Columns"),
+      // ),
+    ),
+  );
 }
